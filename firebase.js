1598495fc836dfde931262ebeae7c081603538ce
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-02 19:27:08 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHWP08Chw4lwaku9y8w_YL6Guh5E5zgXw",
+  authDomain: "pantry-tracking-app-1a660.firebaseapp.com",
+  projectId: "pantry-tracking-app-1a660",
+  storageBucket: "pantry-tracking-app-1a660.appspot.com",
+  messagingSenderId: "503910516783",
+  appId: "1:503910516783:web:7d9aa52991c2593d9d9eda",
+  measurementId: "G-0WNVZF2XLZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
