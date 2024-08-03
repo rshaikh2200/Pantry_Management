360474d6945c8db7cd3f1e56f430c92e09374eda
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-02 23:19:35 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,6 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app); // Initialize Firestore
 export { app, firestore };
