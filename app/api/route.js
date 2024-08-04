ee20c8ddb48acf6f51292a30ef52332cc27b4608
rshaikh2200 <rizwanshaikh2200@gmail.com>
2024-08-04 15:52:46 -0400

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,1 +1,19 @@
-hi
+'use server';
+import Groq from 'groq-sdk';
+
+const groq = new Groq({
+  apiKey: process.env['GROQ_API_KEY'], // This is the default and can be omitted
+});
+
+async function main() {
+  const params: Groq.Chat.CompletionCreateParams = {
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: 'Explain the importance of low latency LLMs' },
+    ],
+    model: 'llama3-8b-8192',
+  };
+  const chatCompletion: Groq.Chat.ChatCompletion = await groq.chat.completions.create(params);
+}
+
+main();
